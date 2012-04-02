79af610c4473edacfd0fea1a968807470b176c61
Sara Steger <sarasteger@gmail.com>
2012-04-02 13:39:22 -0400

    Added userid to tag controller

--- src/Marca/TagBundle/Form/TagType.php
+++ src/Marca/TagBundle/Form/TagType.php
@@ -12,7 +12,6 @@ class TagType extends AbstractType
         $builder
             ->add('name')
             ->add('color')
-            ->add('userid')
             ->add('icon')
             ->add('tagset','entity', array('class'=>'MarcaTagBundle:Tagset', 'property'=>'name','expanded'=>true,'multiple'=>false, )) 
         ;
