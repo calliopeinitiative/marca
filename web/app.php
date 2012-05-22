fa3bdb4b30bc40654c45249bf49fe3edcbd282a9
Ron Balthazor <ron.balthazor@gmail.com>
2012-05-22 12:57:29 -0400

    umask

--- web/app.php
+++ web/app.php
@@ -1,5 +1,5 @@
 <?php
-
+umask(0002);
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
--- web/app_dev.php
+++ web/app_dev.php
@@ -2,7 +2,7 @@
 
 // if you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+umask(0002);
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
