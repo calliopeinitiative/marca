0bb820d2247cd3fb631a1bf2ce7e1190fafe3768
Ron Balthazor <ron.balthazor@gmail.com>
2012-08-28 17:14:13 -0400

    Migrating to S21

--- src/Marca/JournalBundle/Form/JournalType.php
+++ src/Marca/JournalBundle/Form/JournalType.php
@@ -3,17 +3,25 @@
 namespace Marca\JournalBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class JournalType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
             ->add('body')
         ;
     }
+    
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Marca\JournalBundle\Entity\Journal'
+        ));
+    }    
 
     public function getName()
     {
