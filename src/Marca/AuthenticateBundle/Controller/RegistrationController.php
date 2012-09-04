794f9c00da8eb65ed0b014f2aaa0cb1f62aa7b8b
Ron Balthazor <ron.balthazor@gmail.com>
2012-09-04 17:03:51 -0400

    Account creation bug fix

--- src/Marca/AuthenticateBundle/Controller/RegistrationController.php
+++ src/Marca/AuthenticateBundle/Controller/RegistrationController.php
@@ -17,50 +17,5 @@ class RegistrationController extends BaseController
      /**
      * Not yet modified...
      */
-    public function registerAction()
-    {
-        $form = $this->container->get('fos_user.registration.form');
-        $formHandler = $this->container->get('fos_user.registration.form.handler');
-        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
-
-        $process = $formHandler->process($confirmationEnabled);
-        if ($process) {
-            $user = $form->getData();
-
-            if ($confirmationEnabled) {
-                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
-                $route = 'fos_user_registration_check_email';
-            } else {
-                $this->authenticateUser($user);
-                $route = 'fos_user_registration_confirmed';
-            }
-
-            $this->setFlash('fos_user_success', 'registration.flash.user_created');
-            $url = $this->container->get('router')->generate($route);
-
-            return new RedirectResponse($url);
-        }
-
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
-            'form' => $form->createView(),
-            'theme' => $this->container->getParameter('fos_user.template.theme'),
-        ));
-    }
-    
-     /**
-     * Tell the user his account is now confirmed
-      * add functionality to check  and fill profile
-     */
-    public function confirmedAction()
-    {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-        $route = 'user_show';
-        $url = $this->container->get('router')->generate($route);
-
-         return new RedirectResponse($url);
-    }
 
 }
