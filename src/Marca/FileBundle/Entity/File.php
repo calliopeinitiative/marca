dea49be958068da1059089dd5054cf78b082f6b7
Ron Balthazor <ron.balthazor@gmail.com>
2012-12-04 13:50:43 -0500

    Port tweak

--- src/Marca/FileBundle/Entity/File.php
+++ src/Marca/FileBundle/Entity/File.php
@@ -21,7 +21,7 @@ class File
      /**
      * @Assert\File(
      *     maxSize="6M",
-     *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg", "application/pdf", "application/vnd.oasis.opendocument.text"}
+     *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg", "application/pdf", "application/vnd.oasis.opendocument.text", "application/vnd.oasis.opendocument.presentation","application/vnd.oasis.opendocument.spreadsheet", "application/msword", "application/mspowerpoint", "application/excel", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"}
      * )
      * @Vich\UploadableField(mapping="property_file", fileNameProperty="path")
      *
