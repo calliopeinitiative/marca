c1925a66db2cf2b965281bd6e9bbecd077815211
Ron Balthazor <ron.balthazor@gmail.com>
2012-12-05 07:14:23 -0500

    Edit profile name

--- src/Marca/UserBundle/Form/UserType.php
+++ src/Marca/UserBundle/Form/UserType.php
@@ -11,6 +11,8 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('firstname', 'text', array('label'  => 'Firstname','attr' => array('class' => 'span5'),))
+            ->add('lastname', 'text', array('label'  => 'Lastname','attr' => array('class' => 'span5'),))    
             ->add('photo', 'text', array('label'  => 'Photo URL','attr' => array('class' => 'span5'),))
             ->add('bio', 'textarea', array('label'  => 'Tell us a little about youself.',))
         ;
