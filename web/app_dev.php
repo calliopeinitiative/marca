5d8a6299e4b4e8d0d6c7bb401727fa1440e578f5
Ron Balthazor <ron.balthazor@gmail.com>
2013-01-17 16:41:25 -0500

    Cleanup

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
-        '127.0.0.1','192.128.25.45',
+        '127.0.0.1',
         '::1',
     ))
 ) {
