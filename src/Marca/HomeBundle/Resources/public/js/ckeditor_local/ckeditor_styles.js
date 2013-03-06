1f9e401ec329275aaef0de71b23afb2fde180d95
John Weatherford <jweatherford@gmail.com>
2013-03-06 13:05:56 -0500

    Inline note display updates and removal of single space style from CKEditor

--- src/Marca/HomeBundle/Resources/public/js/ckeditor_local/ckeditor_styles.js
+++ src/Marca/HomeBundle/Resources/public/js/ckeditor_local/ckeditor_styles.js
@@ -7,6 +7,5 @@ CKEDITOR.stylesSet.add('marca_styles',
 	[
 	{name:'Paragraph',element:'p',attributes:{'class':'eDoc_paragraph_indent'}},
 	{name:'Double Space',element:'p',attributes:{'class':'eDoc_double_space'}},
-	{name:'Single Space',element:'p',attributes:{'class':'eDoc_single_space'}},
 	{name:'Hanging Indent',element:'p',attributes:{'class':'eDoc_hanging_indent'}}
 	]);
