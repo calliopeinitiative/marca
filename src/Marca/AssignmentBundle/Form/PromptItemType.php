c2a7a0fefc23eb746e928dba0d4d7cb4faeeefab
Robin Wharton <robin.s.wharton@gmail.com>
2013-08-01 13:56:25 -0400

    fixed promptitem form constants

--- src/Marca/AssignmentBundle/Form/PromptItemType.php
+++ src/Marca/AssignmentBundle/Form/PromptItemType.php
@@ -5,7 +5,7 @@ namespace Marca\AssignmentBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Marca\AssignmentBundle\MarcaAssignmentBundle\PromptItem;
+use Marca\AssignmentBundle\Entity\PromptItem;
 
 class PromptItemType extends AbstractType
 {
@@ -14,8 +14,13 @@ class PromptItemType extends AbstractType
         $builder
             ->add('prompt')
             ->add('helpText')
+<<<<<<< Updated upstream
             ->add('type', 'choice', array('choices'   => array(\Marca\AssignmentBundle\Entity\PromptItem::TYPE_SHORTTEXT => 'Short Text', \Marca\AssignmentBundle\Entity\PromptItem::TYPE_PARAGRAPHTEXT => 'Paragraph Text', \Marca\AssignmentBundle\Entity\PromptItem::TYPE_SCALE => 'Scale', \Marca\AssignmentBundle\Entity\PromptItem::TYPE_NORESPONSE => 'No Response'),'required'  => true,'label'  => 'Response Type', 'expanded' => true,'attr' => array('class' => 'checkbox inline'),))   
             ->add('scale','entity',array('class'=>'MarcaAssessmentBundle:Scale','property'=>'name', 'required'=>false))
+=======
+            ->add('type', 'choice', array('choices'   => array(PromptItem::TYPE_SHORTTEXT => 'Short Text', PromptItem::TYPE_PARAGRAPHTEXT => 'Paragraph Text', PromptItem::TYPE_SCALE => 'Scale', PromptItem::TYPE_NORESPONSE => 'No Response'),'required'  => true,'label'  => 'Response Type', 'expanded' => true,'attr' => array('class' => 'checkbox inline'),))   
+            ->add('scale','entity',array('class'=>'MarcaAssessmentBundle:Scale','property'=>'name'))
+>>>>>>> Stashed changes
         ;
     }
 
