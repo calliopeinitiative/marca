9056ce1206c2a3c19842a0236b1b23dc8eaf5e1e
Harrison Massey <harrison.massey@gmail.com>
2014-02-01 22:00:26 -0600

    Revert patch

--- src/Marca/AuthenticateBundle/Controller/RegistrationController.php
+++ src/Marca/AuthenticateBundle/Controller/RegistrationController.php
@@ -15,21 +15,7 @@ use FOS\UserBundle\Model\UserInterface;
 class RegistrationController extends BaseController
 {
      /**
-     * Tell the user to check his email provider
+     * Not yet modified...
      */
-    public function checkEmailAction(Request $request)
-    {
-        $email = $request->query->get('email');
-        $this->container->get('session')->remove('fos_user_send_confirmation_email/email');
-        $user = $this->container->get('fos_user.user_manager')->findUserByEmail($email);
-
-        if (null === $user) {
-            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
-        }
-
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:checkEmail.html.'.$this->getEngine(), array(
-            'user' => $user,
-        ));
-    }
 
 }
