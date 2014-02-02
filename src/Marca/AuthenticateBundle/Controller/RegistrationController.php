61fefdcc9dbd17b2f1a7cb148a0946df00777737
Andrew Famiglietti <afamiglietti@gmail.com>
2014-02-02 10:14:46 -0500

    fix for confirm email 404

--- src/Marca/AuthenticateBundle/Controller/RegistrationController.php
+++ src/Marca/AuthenticateBundle/Controller/RegistrationController.php
@@ -10,12 +10,74 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Exception\AccountStatusException;
 use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
 
 
 class RegistrationController extends BaseController
 {
+    
+    
+    public function registerAction()
+    {
+        $form = $this->container->get('fos_user.registration.form');
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+
+            $authUser = false;
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'new_check_email';
+                $confirmEmail = true;
+            } else {
+                $authUser = true;
+                $route = 'fos_user_registration_confirmed';
+            }
+
+            $this->setFlash('fos_user_success', 'registration.flash.user_created');
+            if ($confirmEmail) {
+                $url = $this->container->get('router')->generate($route, array('email'=>$user->getEmail()));
+            } else {
+                $url = $this->container->get('router')->generate($route);
+            }
+            $response = new RedirectResponse($url);
+
+            if ($authUser) {
+                $this->authenticateUser($user, $response);
+            }
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
+    }
+    
      /**
-     * Not yet modified...
-     */
+     * Tell the user to check his email provider
+      * @Route("/newcheckemail", name="new_check_email")
+     **/
+    public function newCheckEmailAction(Request $request)
+    {
+        $email = $request->query->get('email');
+        $this->container->get('session')->remove('fos_user_send_confirmation_email/email');
+        $user = $this->container->get('fos_user.user_manager')->findUserByEmail($email);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:checkEmail.html.'.$this->getEngine(), array(
+            'user' => $user,
+        ));
+    }
 
 }
