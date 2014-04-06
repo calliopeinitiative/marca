3235f666e905af54e5da0d3d600170da115bf035
Andrew Famiglietti <afamiglietti@gmail.com>
2014-04-06 11:01:00 -0400

    fixed duplicate response send

--- src/Marca/FileBundle/Controller/FileController.php
+++ src/Marca/FileBundle/Controller/FileController.php
@@ -17,8 +17,7 @@ use Marca\FileBundle\Form\ReviewType;
 use Marca\FileBundle\Form\UploadType;
 use Marca\FileBundle\Form\UploadReviewType;
 use Marca\TagBundle\Entity\Tagset;
-use Marca\DocBundle\Entity\Doc;
-
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 /**
  * File controller.
  *
@@ -61,7 +60,6 @@ class FileController extends Controller
         if ($project == 'default') {
             $project = $course->getProjectDefault()->getId();
         }
-
 
         $files = $em->getRepository('MarcaFileBundle:File')->findFilesByProject($project, $user, $scope, $course, $tag, $resource, $byuser, $role);
 
@@ -648,6 +646,9 @@ class FileController extends Controller
      */     
     public function getAction($id)
 	{
+            
+               
+             
              $allowed = array(self::ROLE_INSTRUCTOR, self::ROLE_PORTREVIEW, self::ROLE_STUDENT);
              $this->restrictAccessTo($allowed);
         
@@ -660,8 +661,7 @@ class FileController extends Controller
              $filename = $name.'.'.$ext;
 		
 		$response = new Response();
-
-		$response->setStatusCode(200);
+                $response->setStatusCode(200);
                 switch ($ext) {
                       case "png":
                       $response->headers->set('Content-Type', 'image/png');
@@ -698,6 +698,7 @@ class FileController extends Controller
                       case "docx":
                       $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
                       $response->headers->set('Content-Disposition', 'attachment; filename="'.$filename.'"');
+                      //$response->headers->set('Content-Length: ', $size );
                       break;
                       case "ppt":
                       $response->headers->set('Content-Type', 'application/vnd.mspowerpoint');
@@ -722,9 +723,8 @@ class FileController extends Controller
                       default:
                       $response->headers->set('Content-Type', 'application/octet-stream');
                       }
-		$response->setContent( file_get_contents( $path));
-
-		$response->send();
+                
+                $response->setContent( file_get_contents($path));      
 
 		return $response;
 	}
