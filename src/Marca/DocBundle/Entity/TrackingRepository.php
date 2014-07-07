829a3785bf1d9b2f349fa6191d9eb087b074a621
Ron Balthazor <ron.balthazor@gmail.com>
2014-07-07 16:45:56 -0400

    fix for postgres group by

--- src/Marca/DocBundle/Entity/TrackingRepository.php
+++ src/Marca/DocBundle/Entity/TrackingRepository.php
@@ -16,7 +16,7 @@ class TrackingRepository extends EntityRepository
     public function countMarkup()
     {
         return $this->getEntityManager()
-            ->createQuery('SELECT t.id,m.name,count(t.id)as mcount FROM MarcaDocBundle:Tracking t JOIN t.markup m GROUP BY t.markup')->getResult();
+            ->createQuery('SELECT t.id,m.name,count(t.markup)as mcount FROM MarcaDocBundle:Tracking t JOIN t.markup m GROUP BY t.markup')->getResult();
     }
 
 
