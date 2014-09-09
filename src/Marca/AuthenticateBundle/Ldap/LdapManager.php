42b0648d1ccf3a35f0b3df3d5dd5e9e8d4c6edac
Ron Balthazor <ron.balthazor@gmail.com>
2014-09-09 09:24:10 -0400

    ldap fix

--- src/Marca/AuthenticateBundle/Ldap/LdapManager.php
+++ src/Marca/AuthenticateBundle/Ldap/LdapManager.php
@@ -10,14 +10,26 @@ namespace Marca\AuthenticateBundle\Ldap;
 
 use FR3D\LdapBundle\Ldap\LdapManager as BaseLdapManager;
 use Symfony\Component\Security\Core\User\UserInterface;
+use FR3D\LdapBundle\Driver\LdapDriverInterface;
 
 class LdapManager extends BaseLdapManager
 {
+    private $emailHost;
+
+
+    public function __construct(LdapDriverInterface $driver, $userManager, array $params, $emailHost)
+    {
+        $this->emailHost = $emailHost;
+
+        parent::__construct($driver, $userManager, $params);
+
+    }
+
     protected function hydrate(UserInterface $user, array $entry)
     {
         parent::hydrate($user, $entry);
 
-        $email= $user->getUsername().'@uga.edu';
+        $email= $user->getUsername().'@'.$this->emailHost;
         $user->setEmail($email);
 
     }
