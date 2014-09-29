bc1e2610d7c1afa0cf7cc2b50b443b4bafb168a4
Ron Balthazor <ron.balthazor@gmail.com>
2014-09-29 13:22:45 -0400

    tweaks to fixtures

--- src/Marca/TagBundle/DataFixtures/ORM/LoadTagsetData.php
+++ src/Marca/TagBundle/DataFixtures/ORM/LoadTagsetData.php
@@ -34,7 +34,7 @@ class LoadTagsetData extends AbstractFixture implements OrderedFixtureInterface
         $tagset->setName($name); 
         
         $name = 'Draft1';
-        $color = 'tc1';
+        $color = 'darkorchid';
         $tag1 = new Tag();
         $tag1->setUser($user);
         $tag1->setName($name);
@@ -43,7 +43,7 @@ class LoadTagsetData extends AbstractFixture implements OrderedFixtureInterface
         $tag1->addTagset($tagset);
         
         $name = 'Draft2';
-        $color = 'tc2';
+        $color = 'darkred';
         $tag2 = new Tag();
         $tag2->setUser($user);
         $tag2->setName($name);
@@ -52,7 +52,7 @@ class LoadTagsetData extends AbstractFixture implements OrderedFixtureInterface
         $tag2->addTagset($tagset); 
         
         $name = 'Peer Review';
-        $color = 'tc3';
+        $color = 'darksalmon';
         $tag3 = new Tag();
         $tag3->setUser($user);
         $tag3->setName($name);
@@ -61,7 +61,7 @@ class LoadTagsetData extends AbstractFixture implements OrderedFixtureInterface
         $tag3->addTagset($tagset);         
         
         $name = 'Final';
-        $color = 'tc4';
+        $color = 'darkseagreen';
         $tag4 = new Tag();
         $tag4->setUser($user);
         $tag4->setName($name);
@@ -69,21 +69,12 @@ class LoadTagsetData extends AbstractFixture implements OrderedFixtureInterface
         $tag4->setShared($shared);
         $tag4->addTagset($tagset); 
         
-        $name = 'Backup';
-        $color = 'tc3';
-        $tag5 = new Tag();
-        $tag5->setUser($user);
-        $tag5->setName($name);
-        $tag5->setColor($color);
-        $tag5->setShared($shared);
-        $tag5->addTagset($tagset);
-        
+
         $manager->persist($tagset);       
         $manager->persist($tag1);
         $manager->persist($tag2);
         $manager->persist($tag3);
         $manager->persist($tag4);
-        $manager->persist($tag5);        
         $manager->flush();
 
         $this->addReference('tagset', $tagset);
