60ec062dcee294c4d834633838ea2430df90065c
Ron Balthazor <ron.balthazor@gmail.com>
2014-11-24 06:35:37 -0500

    delete modal filter test

--- src/Marca/HomeBundle/Twig/MarcaExtension.php
+++ src/Marca/HomeBundle/Twig/MarcaExtension.php
@@ -15,7 +15,8 @@ class MarcaExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('html_raw', array($this, 'filterHtml'), array('is_safe' => array('html')))
+            new \Twig_SimpleFilter('html_raw', array($this, 'filterHtml'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFilter('delete_modal', array($this, 'filterDelete_modal'), array('is_safe' => array('html')))
         );
     }
 
@@ -24,6 +25,29 @@ class MarcaExtension extends \Twig_Extension
         $html_raw = $string;
         return $html_raw;
     }
+
+    public function filterDelete_modal($string)
+    {
+        $html_raw = <<<EOF
+    <div id="delete_modal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <span class="modal-title">Delete</span>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to delete?</p>
+                        $string
+                    </div>
+            </div>
+        </div>
+    </div>
+EOF;
+
+        return $html_raw;
+    }
+
 
     public function getName()
     {
