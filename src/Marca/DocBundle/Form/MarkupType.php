7c5df4f70a15248ac87af1f2993dbc22cb74e845
Ron Balthazor <rlbaltha@uga.edu>
2016-08-26 06:36:51 -0400

    parameterized cas config

--- src/Marca/DocBundle/Form/MarkupType.php
+++ src/Marca/DocBundle/Form/MarkupType.php
@@ -14,8 +14,8 @@ class MarkupType extends AbstractType
         $builder
             ->add('name','text', array('label'  => 'Name','attr' => array('class' => 'text form-control'),))
             ->add('color','text', array('attr' => array('class' => 'colorpicker'),'label'  => 'Markup Color',) )
-            ->add('url','text', array('label'  => 'Url (Web address) for outside resources','attr' => array('class' => 'text form-control'),))
-            ->add('linktext','text', array('label'  => 'Display text for link to outside resources','attr' => array('class' => 'text form-control'),))
+            ->add('url','text', array('label'  => 'Url (Web address) for outside resources','attr' => array('class' => 'text form-control'), 'required' => false))
+            ->add('linktext','text', array('label'  => 'Display text for link to outside resources','attr' => array('class' => 'text form-control'),  'required' => false))
             ->add('mouseover', 'ckeditor', array('label'=>'Text to display for this tag','config_name' => 'editor_docs',))
         ;
     }
