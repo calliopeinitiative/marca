39d45c25a69ee83a2cf2d34e985f2d173894d492
Ron Balthazor <rlbaltha@uga.edu>
2018-02-08 14:05:35 -0500

    assetic fix

---
+++ src/Marca/HomeBundle/Resources/public/ckeditor/ckeditor_styles.js
@@ -0,0 +1,18 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.stylesSet.add('marca_styles',
+	[
+	{name:'Title',element:'p',attributes:{'class':'eDoc_title_pdf'}},
+	{name:'Works Cited Title',element:'p',attributes:{'class':'eDoc_wc_pdf'}},
+	{name:'Header',element:'p',attributes:{'class':'eDoc_header_pdf'}},
+	{name:'Paragraph',element:'p',attributes:{'class':'eDoc_paragraph_indent'}},
+    {name:'Single Space',element:'p',attributes:{'class':'eDoc_single_space'}},
+	{name:'Double Space',element:'p',attributes:{'class':'eDoc_double_space'}},
+	{name:'Hanging Indent',element:'p',attributes:{'class':'eDoc_hanging_indent'}},
+        {name:'h2',element:'h2'},
+        {name:'h3',element:'h3'},
+        {name:'h4',element:'h4'},        
+	]);
