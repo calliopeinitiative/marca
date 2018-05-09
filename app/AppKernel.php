9f0de693b0c900c6fa575c179efacd22f75a5101
Ron Balthazor <rlbaltha@uga.edu>
2018-05-09 15:37:41 -0400

    remove fixtures from appkernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -44,7 +44,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
