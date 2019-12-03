8781fc149d80eedb98a07b698399b1966c6300a5
Ron Balthazor <rlbaltha@uga.edu>
2019-12-03 12:10:48 -0500

    promptitem fix

--- src/Marca/AssignmentBundle/Controller/PromptItemController.php
+++ src/Marca/AssignmentBundle/Controller/PromptItemController.php
@@ -5,7 +5,7 @@ namespace Marca\AssignmentBundle\Controller;
 use Marca\AssignmentBundle\Entity\PromptItem;
 use Marca\AssignmentBundle\Form\PromptItemType;
 use Marca\HomeBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,7 +21,6 @@ class PromptItemController extends Controller
      * Lists all PromptItem entities.
      *
      * @Route("/", name="promptitem")
-     * @Template()
      */
     public function indexAction()
     {
@@ -29,16 +28,15 @@ class PromptItemController extends Controller
 
         $entities = $em->getRepository('MarcaAssignmentBundle:PromptItem')->findAll();
 
-        return array(
+        return $this->render('MarcaAssignmentBundle:PromptItem:index.html.twig', array(
             'entities' => $entities,
-        );
+        ));
     }
 
     /**
      * Finds and displays a PromptItem entity.
      *
-     * @Route("/{id}/show", name="promptitem_show")
-     * @Template()
+     * @Route("/{id}/show", name="promptitem_show", methods={"GET"})
      */
     public function showAction($id)
     {
@@ -52,17 +50,16 @@ class PromptItemController extends Controller
 
         $deleteForm = $this->createDeleteForm($id);
 
-        return array(
+        return $this->render('MarcaAssignmentBundle:PromptItem:show.html.twig', array(
             'entity'      => $entity,
             'delete_form' => $deleteForm->createView(),
-        );
+        ));
     }
 
     /**
      * Displays a form to create a new PromptItem entity.
      *
-     * @Route("/{reviewrubricid}/new", name="promptitem_new")
-     * @Template()
+     * @Route("/{reviewrubricid}/new", name="promptitem_new", methods={"GET"})
      */
     public function newAction($reviewrubricid)
     {
@@ -74,20 +71,19 @@ class PromptItemController extends Controller
 
         $promptitem->setType(0);
 
-        $form   = $this->createForm(new PromptItemType(), $promptitem);
+        $form   = $this->createForm(PromptItemType::class, $promptitem);
 
-        return array(
+        return $this->render('MarcaAssignmentBundle:PromptItem:new.html.twig', array(
             'promptitem' => $promptitem,
             'reviewrubric' => $reviewrubric,
             'form'   => $form->createView(),
-        );
+        ));
     }
 
     /**
      * Creates a new PromptItem entity.
      *
-     * @Route("/{reviewrubricid}/create", name="promptitem_create")
-     * @Method("POST")
+     * @Route("/{reviewrubricid}/create", name="promptitem_create", methods={"POST"})
      * @Template("MarcaAssignmentBundle:PromptItem:new.html.twig")
      */
     public function createAction(Request $request,$reviewrubricid)
@@ -101,7 +97,7 @@ class PromptItemController extends Controller
         $promptitem->setReviewRubric($reviewrubric);
         $promptitem->setSortOrder($count + 1);
        
-        $form = $this->createForm(new PromptItemType(), $promptitem);
+        $form = $this->createForm(PromptItemType::class, $promptitem);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
@@ -112,17 +108,16 @@ class PromptItemController extends Controller
             return $this->redirect($this->generateUrl('reviewrubric_show', array('id' => $reviewrubricid)));
         }
 
-        return array(
+        return $this->render('MarcaAssignmentBundle:PromptItem:new.html.twig', array(
             'promptitem' => $promptitem,
             'form'   => $form->createView(),
-        );
+        ));
     }
 
     /**
      * Displays a form to edit an existing PromptItem entity.
      *
-     * @Route("/{id}/edit", name="promptitem_edit")
-     * @Template()
+     * @Route("/{id}/edit", name="promptitem_edit", methods={"GET"})
      */
     public function editAction($id)
     {
@@ -134,22 +129,21 @@ class PromptItemController extends Controller
             throw $this->createNotFoundException('Unable to find PromptItem entity.');
         }
 
-        $editForm = $this->createForm(new PromptItemType(), $promptitem);
+        $editForm = $this->createForm(PromptItemType::class, $promptitem);
         $deleteForm = $this->createDeleteForm($id);
 
-        return array(
+        return $this->render('MarcaAssignmentBundle:PromptItem:edit.html.twig', array(
             'promptitem'      => $promptitem,
             'edit_form'   => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
-        );
+        ));
+
     }
 
     /**
      * Edits an existing PromptItem entity.
      *
-     * @Route("/{id}/update", name="promptitem_update")
-     * @Method("POST")
-     * @Template("MarcaAssignmentBundle:PromptItem:edit.html.twig")
+     * @Route("/{id}/update", name="promptitem_update", methods={"POST"})
      */
     public function updateAction(Request $request, $id)
     {
@@ -162,7 +156,7 @@ class PromptItemController extends Controller
         }
 
         $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new PromptItemType(), $entity);
+        $editForm = $this->createForm(PromptItemType::class, $entity);
         $editForm->handleRequest($request);
 
         if ($editForm->isValid()) {
@@ -173,18 +167,18 @@ class PromptItemController extends Controller
             return $this->redirect($this->generateUrl('reviewrubric_show', array('id' => $rubricid)));
         }
 
-        return array(
+        return $this->render('MarcaAssignmentBundle:PromptItem:edit.html.twig', array(
             'entity'      => $entity,
             'edit_form'   => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
-        );
+        ));
     }
 
 
     /**
      * Moves a Project entity up one in the display order.
      *
-     * @Route("/{promptitemId}/{previousPromptitemId}/promote", name="promptitem_promote")
+     * @Route("/{promptitemId}/{previousPromptitemId}/promote", name="promptitem_promote", methods={"GET"})
      */
     public function promoteAction($promptitemId, $previousPromptitemId)
     {
@@ -210,7 +204,7 @@ class PromptItemController extends Controller
     /**
      * Moves a Project entity down one in the display order.
      *
-     * @Route("/{promptitemId}/{followingPromptitemId}/demote", name="promptitem_demote")
+     * @Route("/{promptitemId}/{followingPromptitemId}/demote", name="promptitem_demote", methods={"GET"})
      */
     public function demoteAction($promptitemId, $followingPromptitemId)
     {
@@ -236,8 +230,7 @@ class PromptItemController extends Controller
     /**
      * Displays a form to delete an existing PromptItem entity.
      *
-     * @Route("/{id}/delete_modal", name="promptitem_delete_modal")
-     * @Template()
+     * @Route("/{id}/delete_modal", name="promptitem_delete_modal", methods={"GET"})
      */
     public function delete_modalAction($id)
     {
@@ -251,17 +244,16 @@ class PromptItemController extends Controller
 
         $deleteForm = $this->createDeleteForm($id);
 
-        return array(
+        return $this->render('MarcaAssignmentBundle:PromptItem:edit.html.twig', array(
             'promptitem'      => $promptitem,
             'delete_form' => $deleteForm->createView(),
-        );
+        ));
     }
 
     /**
      * Deletes a PromptItem entity.
      *
-     * @Route("/{id}/delete", name="promptitem_delete")
-     * @Method("POST")
+     * @Route("/{id}/delete", name="promptitem_delete", methods={"DELETE"})
      */
     public function deleteAction(Request $request, $id)
     {
@@ -287,7 +279,7 @@ class PromptItemController extends Controller
     private function createDeleteForm($id)
     {
         return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
+            ->add('id', HiddenType::class)
             ->getForm()
         ;
     }
--- src/Marca/AssignmentBundle/Form/PromptItemType.php
+++ src/Marca/AssignmentBundle/Form/PromptItemType.php
@@ -2,7 +2,9 @@
 
 namespace Marca\AssignmentBundle\Form;
 
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Marca\AssignmentBundle\Entity\PromptItem;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,10 +15,16 @@ class PromptItemType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('prompt', 'ckeditor', array('config_name' => 'editor_simple',))
-            ->add('helpText', 'ckeditor', array('config_name' => 'editor_simple',))
-            ->add('type', ChoiceType::class, array('choices'   => array(PromptItem::TYPE_SHORTTEXT => 'Short Text', PromptItem::TYPE_PARAGRAPHTEXT => 'Paragraph Text', PromptItem::TYPE_SCALE => 'Scale', PromptItem::TYPE_NORESPONSE => 'No Response'),'required'  => true,'label'  => 'Response Type', 'expanded' => true,'attr' => array('class' => 'radio'),))
-            ->add('scale','entity',array('class'=>'MarcaAssessmentBundle:Scale','property'=>'name', 'expanded' => true,'required'  => true,'attr' => array('class' => 'radio'),))        ;
+            ->add('prompt', CKEditorType::class, array('config_name' => 'editor_simple',))
+            ->add('helpText', CKEditorType::class, array('config_name' => 'editor_simple',))
+            ->add('type', ChoiceType::class, array('choices'   =>
+                array(
+                    'Short Text'=> PromptItem::TYPE_SHORTTEXT ,
+                    'Paragraph Text' =>PromptItem::TYPE_PARAGRAPHTEXT ,
+                    'Scale' => PromptItem::TYPE_SCALE,
+                    'No Response'=> PromptItem::TYPE_NORESPONSE
+                ),'required'  => true,'label'  => 'Response Type', 'expanded' => true,'attr' => array('class' => 'radio'),))
+            ->add('scale',EntityType::class ,array('class'=>'MarcaAssessmentBundle:Scale','choice_label'=>'name', 'expanded' => true,'required'  => true,'attr' => array('class' => 'radio'),))        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
